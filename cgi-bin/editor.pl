db7cae63f946f0b5128f062adf5a0f525f6fd3af
leonhatches <jhatches@unsa.edu.pe>
2024-12-05 11:02:54 -0500

    Estructura del editor de paginas

---
+++ cgi-bin/editor.pl
@@ -0,0 +1,58 @@
+#!/Strawberry/perl/bin/perl.exe
+#/usr/bin/perl
+
+# Modulos
+use strict;
+use warnings;
+use CGI;
+use DBI;
+use utf8;
+use Encode;
+
+#Configuración de conexión
+my $database = "paginas";
+my $hostname = "mariadb2";
+my $port = 3307;
+my $user = "cgi_user";
+my $password = "123456789";
+
+#DSN de conexión
+my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
+
+#Conexión a la DB
+my $ dbh = DBI->connect($dsn, $user, $password, {
+	RaiseError => 1,
+	PrintError => 0,
+	mysql_enable_utf8 => 1,
+});
+
+#Consulta de lineas de la página creada
+
+# CGI
+my $cgi = CGI->new;
+print $cgi->header('text/html');
+      $cgi->charset('UTF-8');
+
+print<<HTML;
+<!DOCTYPE html>
+<html>
+  	<head>
+		<!--fuente de letra-->
+		<link
+			href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+			rel="stylesheet"
+			type="text/css">
+
+		<!--CSS-->
+		<link rel = "stylesheet" type = "text/css" href = "/css/style.css">
+    	
+		<!--Style-->
+		<style type="text/css">
+			body { margin: 0px 0px 100px; }
+		</style>
+
+		<title>Editor</title>
+	</head>
+	
+	<body>	
+HTML
