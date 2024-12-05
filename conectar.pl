e2cc78e4cff23385f580b07ed7c920d11603073d
Andhyb <achipanamam@unsa.edu.pe>
2024-12-05 18:57:41 -0500

    Probando la conexion a la base de datos

--- conectar.pl
+++ conectar.pl
@@ -3,3 +3,21 @@
 use strict;
 use warnings;
 use DBI;
+
+my $database = "wikipweb1";
+my $host     = "127.0.0.1";
+my $port     = 3306;
+my $user     = "root";
+my $password = "";
+
+my $dsn = "DBI:mysql:database=$database;host=$host;port=$port";
+
+my $dbh = DBI->connect($dsn, $user, $password, { RaiseError => 1, PrintError => 0 });
+
+if ($dbh) {
+    print "Conexión exitosa a MariaDB\n";
+    $dbh->disconnect();
+} else {
+    die "Error al conectar a MariaDB: " . DBI->errstr;
+}
+
