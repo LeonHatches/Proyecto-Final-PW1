3265d2e950bb728894e4f7a686dcba5a17043320
Andhyb <achipanamam@unsa.edu.pe>
2024-12-05 19:44:14 -0500

    Version final de la conexion a la base de datos

--- conectar.pl
+++ conectar.pl
@@ -4,9 +4,22 @@ use strict;
 use warnings;
 use DBI;
 
-print "Content-type: text/plain\n\n";
+print "Content-type: text/html\n\n";
+print <<END_HTML;
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>WikiPweb</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+END_HTML
 
-# Datos de conexión
+print <<END_HTML;
+<a  id="btn-agg" href='agregar.pl'><button >Agregar Nuevo</button></a>
+END_HTML
+
 my $database = "wikipweb1";
 my $host     = "127.0.0.1";
 my $port     = 3306;
@@ -14,23 +27,43 @@ my $user     = "root";
 my $password = "";
 
 my $dsn = "DBI:mysql:database=$database;host=$host;port=$port";
-my $dbh = DBI->connect($dsn, $user, $password, { RaiseError => 1, PrintError => 0 });
+
+my $dbh = DBI->connect($dsn, $user, $password, { PrintError => 0, RaiseError => 1 });
 
 if ($dbh) {
-    print "Conexión exitosa.\n";
-
-    # Realizar consulta
+    # consultas a  la tabla wiki de bd
     my $sql = "SELECT id, titulo FROM wiki";
     my $sth = $dbh->prepare($sql);
     $sth->execute();
 
-    # Mostrar resultados
+    print "<table>\n";
+    print "  <tr>\n";
+    print "    <th>ID</th>\n";
+    print "    <th>Título</th>\n";
+    print "    <th>Acciones</th>\n";
+    print "  </tr>\n";
+    binmode(STDOUT, ":utf8");
     while (my $row = $sth->fetchrow_hashref) {
-        print "ID: $row->{id}, Título: $row->{titulo}\n";
+        print "  <tr>\n";
+        print "    <td>$row->{id}</td>\n";
+        print "    <td>$row->{titulo}</td>\n";
+        print "    <td class='actions'>\n";
+        print "      <a href='ver.pl?id=$row->{id}'>V</a>\n";
+        print "      <a href='editar.pl?id=$row->{id}'>E</a>\n";
+        print "      <a href='eliminar.pl?id=$row->{id}'>X</a>\n";
+        print "    </td>\n";
+        print "  </tr>\n";
     }
+
+    print "</table>\n";
 
     $sth->finish();
     $dbh->disconnect();
 } else {
-    die "Error al conectar a la base de datos: " . DBI->errstr;
+    die "Error al conectar a la base de datos de MariaDB: " . DBI->errstr;
 }
+
+print <<END_HTML;
+</body>
+</html>
+END_HTML
