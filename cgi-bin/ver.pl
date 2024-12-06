479564f9fd7be08e35d4c7662a1faa9cd698b51f
Gael_ <ghuamanimac@unsa.edu.pe>
2024-12-05 20:26:26 -0500

    subrutina de markdown a html

--- cgi-bin/ver.pl
+++ cgi-bin/ver.pl
@@ -8,10 +8,12 @@ my $cgi = CGI->new;
 #ID DE LA PÁGINA
 my $page_id = $cgi->param('page') || 'default_page';
 #CONEXIÓN A LA BASE DE DATOS...
+my $dbh = DBI->connect()
 #consultar contenido
 my $sth = $dbh->prepare("Selecciona el contenido de las páginas donde id = ?");
 $sth->execute($page_id);
 #contenido de markdown
+my ($markdown_contenido) = $sth->fetchrow_array;
 #markdown a html
 my $html_contenido = markdown_a_html($markdown_contenido);
 print $cgi->header(-type => 'text/html');
@@ -30,5 +32,17 @@ print <<HTML;
 </html>
 HTML
 sub markdown_a_html{
-  return $markdown;
+    my ($markdown) = @_;
+        $markdown =~ s/^###### (.*?)$/<h6>$1<\/h6>/gm;  # Encabezado H6
+        $markdown =~ s/^##### (.*?)$/<h5>$1<\/h5>/gm;  # Encabezado H5
+        $markdown =~ s/^#### (.*?)$/<h4>$1<\/h4>/gm;  # Encabezado H4
+        $markdown =~ s/^### (.*?)$/<h3>$1<\/h3>/gm;  # Encabezado H3
+        $markdown =~ s/^## (.*?)$/<h2>$1<\/h2>/gm;  # Encabezado H2
+        $markdown =~ s/^# (.*?)$/<h1>$1<\/h1>/gm;   # Encabezado H1
+        $markdown =~ s/\*\*(.*?)\*\*/<b>$1<\/b>/g;  # Texto en negrita
+        $markdown =~ s/\*(.*?)\*/<i>$1<\/i>/g;      # Texto en cursiva
+        $markdown =~ s/~~(.*?)~~/<del>$1<\/del>/g;  # Texto tachado
+        $markdown =~ s/\[(.*?)\]\((.*?)\)/<a href="$2">$1<\/a>/g;  # Hipervínculos
+        $markdown =~ s/`(.*?)`/<code>$1<\/code>/g;  # Código en línea
+    return $markdown;
 }
