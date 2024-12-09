8357921cddf076863ca1b1d1ef800f15f55a2e79
leonhatches <jhatches@unsa.edu.pe>
2024-12-09 17:42:42 -0500

    Editor modificado con css

--- cgi-bin/editor.pl
+++ cgi-bin/editor.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
+
 # Modulos
 use strict;
 use warnings;
@@ -38,7 +39,16 @@ my $ dbh = DBI->connect($dsn, $user, $password, {
 print<<HTML;
 <!DOCTYPE html>
 <html>
-  	<head>
+
+	<head>
+		<!--Extensión para caracteres especiales-->
+		<meta charset="utf-8">
+
+		<!--fuente de letra-->
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
+			  rel="stylesheet"
+		      type="text/css">
+
 		<!--fuente de letra-->
 		<link
 			href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
@@ -47,39 +57,34 @@ print<<HTML;
 
 		<!--CSS-->
 		<link rel = "stylesheet" type = "text/css" href = "/css/style.css">
-    	
-		<title>Editor</title>
+
+		<title>EDITAR</title>
 	</head>
 	
 	<body>
+
 		<!--CABECERA-->
-		<header>
-    		<nav>
-        		<ul>
-        			<li><a href="/index.html">INICIO</a></li>
-        			<li><a href="lista.pl">LISTA</a></li>
-        		</ul>
-    		</nav>
+		
+		<header> <a class="logo" href="index.html">Wikipedia</a>
+		  <nav>
+			<ul>
+			  <li><a href="/index.html#integrantes">Inicio</a></li>
+			  <li><a href="lista.pl">Ver lista</a></li>
+			</ul>
+		  </nav>
 		</header>
 
-
-		<!--CAJA DE TITULO-->
-		<div>
-			EDITOR DE PÁGINA
-		</div>
-
-		<p>
-			Funcionalidades-agregar-tabla
-		</p>
-
-		<!--EDITOR DE PAGINA-->
-		<div>	
+		<!--CONTENEDOR NUEVA PÁGINA-->
+		<div class="container">
+			<div id="crear-pagina" class="contenedor-nuevapagina"
+				style="margin: 0; border-top-right-radius: 0px; border-top-left-radius: 0px;">
+				<h2>EDITANDO LA <span class="textos-rojos">PÁGINA</span></h2>
 HTML
 
 
 if ($dbh)
 {
-	print "\t\t\t<form action=\"./conexion.pl\" method=\"GET\">\n";
+	print "\t\t\t\t\t<form action=\"./conexion.pl\" method=\"GET\" class=\"formulario-estilizado\">\n";
 	
 	my $query = "SELECT titulo, texto FROM wiki WHERE id = ?";
 	my $sth = $dbh->prepare($query);
@@ -97,12 +102,17 @@ if ($dbh)
 	}
 
 	print<<HTML;
-			<input type="hidden" name="direccion" value="editor">
-			<input type="text" name="titulo" value="$titulo">
-			<input type="text" name="texto" value="$texto">
-			<input type="hidden" name='id' value='$id'>
-			<input type="submit" value="Enviar">
-			
+						<input type="hidden" name="direccion" value="editor">
+
+						<label for="titulo">Título:</label>
+						<input type="text" id="titulo" name="titulo" value="$titulo">
+
+						<label for="texto">Contenido:</label>
+						<textarea id="texto" name="texto">$texto</textarea>
+						
+						<input type="hidden" name='id' value='$id'>
+						<input type="submit" value="Enviar" class="btn-accion btn-crear">
+				</form>		
 HTML
 
     $sth->finish();
@@ -114,6 +124,82 @@ else {
 }
 
 print<<HTML;
+			</div>
+		</div>
+
+		<!--TABLA DE FUNCIONALIDADES-->
+		<div class="tabla-markdown-container">
+			<div class="titulo-funcionalidad">
+				<h2>Funcionalidades<span class="textos-rojos"> Markdown</span></h2>
+			</div>
+				<p>Esta tabla muestra cómo se convierten los elementos de Markdown a HTML y su salida renderizada.</p>
+				<table class="tabla-markdown">
+					<thead>
+						<tr>
+							<th>Elemento</th>
+							<th>Markdown</th>
+							<th>HTML</th>
+							<th>Resultado</th>
+						</tr>
+					</thead>
+					<tbody>
+						<!-- Encabezados -->
+						<tr>
+							<td>Encabezado o heading</td>
+							<td>
+								# Encabezado 1<br>
+								## Encabezado 2<br>
+								###### Encabezado 6
+							</td>
+							<td>
+								&lt;h1&gt;Encabezado 1&lt;/h1&gt;<br>
+								&lt;h2&gt;Encabezado 2&lt;/h2&gt;<br>
+								&lt;h6&gt;Encabezado 6&lt;/h6&gt;
+							</td>
+							<td>
+								<h1>Encabezado 1</h1>
+								<h2>Encabezado 2</h2>
+								<h6>Encabezado 6</h6>
+							</td>
+						</tr>
+
+						<!-- Estilos de texto -->
+						<tr>
+							<td>Estilos de texto</td>
+							<td>
+								***Texto negrita y cursiva***<br>
+								**Texto negrita**<br>
+							  *Texto cursiva*
+						  </td>
+							<td>
+								&lt;strong&gt;&lt;em&gt;Texto&lt;/em&gt;&lt;/strong&gt;<br>
+								&lt;strong&gt;Texto&lt;/strong&gt;<br>
+								&lt;em&gt;Texto&lt;/em&gt;
+							</td>
+							<td>
+								<strong><em>Texto</em></strong><br>
+								<strong>Texto</strong><br>
+								<em>Texto</em>
+							</td>
+						</tr>
+
+						<!-- Enlaces -->
+						<tr>
+							<td>Enlaces</td>
+							<td>[Texto](https://example.com)</td>
+							<td>&lt;a href="https://example.com"&gt;Texto&lt;/a&gt;</td>
+							<td><a href="https://example.com">Texto</a></td>
+						</tr>
+
+						<!-- Saltos de línea -->
+						<tr>
+							<td>Saltos de línea</td>
+							<td>Línea 1<br>Línea 2</td>
+							<td>Línea 1&lt;br&gt;Línea 2</td>
+							<td>Línea 1<br>Línea 2</td>
+						</tr>
+					</tbody>
+				</table>
 		</div>
 	</body>
 HTML
