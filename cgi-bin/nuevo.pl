6b27c39cf86d5ffc83ac9a8430f033a0aea5a4fb
Andhyb <achipanamam@unsa.edu.pe>
2024-12-19 15:40:59 -0500

    Version sencilla de prueba que imprime parametros sin conectar aun a la bd

--- cgi-bin/nuevo.pl
+++ cgi-bin/nuevo.pl
@@ -1,1 +1,25 @@
 
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+
+my $cgi = CGI->new;
+print $cgi->header('application/xml');
+print "<?xml version='1.0' encoding='utf-8'?>\n";
+
+my $titulo = $cgi->param('titulo');
+my $texto = $cgi->param('text_intro');
+my $user = $cgi->param('owner'); 
+
+if (defined $titulo and defined $texto and defined $user) {
+    print <<XML;
+<article>
+    <title>$titulo</title>
+    <text>$texto</text>
+    <owner>$user</owner>
+</article>
+XML
+} else {
+    print "<error>Faltan parámetros</error>\n";
+}
