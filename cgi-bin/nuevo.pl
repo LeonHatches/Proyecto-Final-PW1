115352eb5d85b0391614860d9f43a3dfb6f56283
Andhyb <achipanamam@unsa.edu.pe>
2024-12-19 15:48:48 -0500

    Conexion a la bd y verificacion de articulo (si ya existe uno con el mismo nombre y propietario o no)

--- cgi-bin/nuevo.pl
+++ cgi-bin/nuevo.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use CGI;
+use DBI;
 
 my $cgi = CGI->new;
 print $cgi->header('application/xml');
@@ -10,16 +11,26 @@ print "<?xml version='1.0' encoding='utf-8'?>\n";
 
 my $titulo = $cgi->param('titulo');
 my $texto = $cgi->param('text_intro');
-my $user = $cgi->param('owner'); 
+my $user = $cgi->param('owner');
 
 if (defined $titulo and defined $texto and defined $user) {
-    print <<XML;
-<article>
-    <title>$titulo</title>
-    <text>$texto</text>
-    <owner>$user</owner>
-</article>
-XML
+    my $dsn = "DBI:mysql:database=wikipweb1;host=localhost;port=3306";
+    my $dbh = DBI->connect($dsn, 'root', '1234567890', {
+        RaiseError => 1,
+        PrintError => 0,
+    });
+
+    my $check_sql = "SELECT * FROM Articles WHERE title = ? AND owner = ?";
+    my $check_sth = $dbh->prepare($check_sql);
+    $check_sth->execute($titulo, $user);
+
+    if ($check_sth->fetchrow_array) {
+        print "<error>El artículo ya existe</error>\n";
+    } else {
+        print "<message>Artículo no encontrado</message>\n";
+    }
+
+    $dbh->disconnect;
 } else {
     print "<error>Faltan parámetros</error>\n";
 }
