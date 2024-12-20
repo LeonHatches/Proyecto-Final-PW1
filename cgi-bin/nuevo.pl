364924ad7e1e5a4999530eda5cebc2681fb7c01e
leonhatches <jhatches@unsa.edu.pe>
2024-12-20 10:32:45 -0500

    Version funcional de nuevo.html

--- cgi-bin/nuevo.pl
+++ cgi-bin/nuevo.pl
@@ -1,5 +1,5 @@
 #!/Strawberry/perl/bin/perl.exe
-#!/usr/bin/perl
+#/usr/bin/perl
 
 use strict;
 use warnings;
@@ -12,62 +12,83 @@ print $cgi->header('application/xml');
 print "<?xml version='1.0' encoding='utf-8'?>\n";
 
 # Obtener los parámetros
-my $titulo = $cgi->param('titulo');
-my $texto = $cgi->param('text_intro');
-my $user = $cgi->param('owner'); # El "owner" viene del login.pl
+my $title = $cgi->param('title');
+my $text = $cgi->param('text');
+my $owner = $cgi->param('owner'); # El "owner" viene del login.pl
 
-# Verificar que se recibieron los datos necesarios
-if (defined $user and defined $titulo and defined $texto) {
-    # Configuración de conexión a la base de datos
-    my $database = 'wikipweb1';
-    my $hostname = 'localhost';
-    my $port = 3306;
-    my $db_user = 'root';
-    my $db_password = '1234567890';
-    my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
+#-----------------------------------------------------------------
 
-    # Conexión a la base de datos
-    my $dbh = DBI->connect($dsn, $db_user, $db_password, {
+# Configuración de conexión
+my $db = 'wikipweb1';
+my $db_host = 'localhost';
+my $db_port = 3306;
+my $db_user = 'root';
+my $db_password = '1234567890';
+
+# DSN de conexión
+my $dsn = "DBI:mysql:database=$db;host=$db_host;port=$db_port";
+
+# Conexión a la base de datos
+my $dbh = DBI->connect(
+    $dsn, 
+    $db_user, 
+    $db_password, 
+    {
         RaiseError => 1,
         PrintError => 0,
-        mysql_enable_utf8 => 1,
-    });
+        AutoCommit => 1,
+        mysql_enable_utf8mb4 => 1
+    }
+) or die "Error al conectar a la base de datos MariaDB: $DBI::errstr\n";
+
+$dbh->do("SET NAMES utf8mb4");
+
+#-----------------------------------------------------------------
+
+# Verificar que se recibieron los datos necesarios
+if (defined $owner and defined $title and defined $text) {
 
     # Comprobar si ya existe un artículo con el mismo título y propietario
     my $check_sql = "SELECT * FROM Articles WHERE title = ? AND owner = ?";
     my $check_sth = $dbh->prepare($check_sql);
-    $check_sth->execute($titulo, $user);
+    $check_sth->execute($title, $owner);
 
     if ($check_sth->fetchrow_array) {
-        # Ya existe un artículo con ese título y propietario
-        print "<user>\n</user>\n";
+        XMLvacio();
     } else {
         # Insertar un nuevo artículo
-        my $sql = "INSERT INTO Articles (title, owner, content) VALUES (?, ?, ?)";
+        my $sql = "INSERT INTO Articles (owner, title, text) VALUES (?, ?, ?)";
         my $sth = $dbh->prepare($sql);
-        $sth->execute($titulo, $user, $texto);
+        $sth->execute($owner, $title, $text);
 
         # Finalizar la operación
         $sth->finish;
         $dbh->disconnect;
 
         # Mostrar éxito
-        successLogin($titulo, $user, $texto);
+        successLogin($title, $text);
     }
 } else {
-    # Parámetros faltantes
-    print "<user>\n</user>\n";
+     XMLvacio();
 }
 
 # Subrutina para imprimir el XML de éxito
 sub successLogin {
-    my ($titulo, $user, $texto) = @_;
-    print <<XML;
+    my ($title, $text) = @_;
+    print<<XML;
 <article>
-    <title>$titulo</title>
-    <text>$texto</text>
-    <owner>$user</owner>
+    <title>$title</title>
+    <text>$text</text>
 </article>
 XML
 }
 
+# Subrutina para imprimir el XML vacio
+sub XMLvacio {
+    print<<XML;
+<article>
+    <title></title>
+    <text></text>
+</article>
+XML
+}
